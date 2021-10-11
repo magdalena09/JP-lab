9753036fd9a1699d221362abf3eb7e37d5471726
Magdalena <61758379+magdalena09@users.noreply.github.com>
2021-10-11 17:45:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main{
     public static void main(String[] args) {
-        int max = 10, multi, zero = 0;
+       int max = 10, multi;
         for (int i = 1; i <= max; i++) {
             for (int j = 1; j <= max; j++) {
                 multi = i * j;
                 if (multi<10) {
-                    System.out.print(" " + zero + multi + " ");
+                    System.out.print(multi + "   ");
                 }else
                     System.out.print(multi + "  ");
             }
             System.out.println();
-	}
+        }
     }
 }
