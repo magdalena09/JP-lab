87f8431665da53cc1837e691269c69845e8de139
magdalena09 <magdalena.glowacka@o2.pl>
2021-12-07 14:05:26 +0100

    Finished

--- lab04/src/lab4/Circle.java
+++ lab04/src/lab4/Circle.java
@@ -3,10 +3,12 @@ package lab4;
 import java.awt.*;
 
 public class Circle extends Draw{
+
     @Override
     public void draw(Graphics2D g2d) {
         g2d.fillOval(x,y,50,50);
     }
+
     public Circle(int x, int y){
         super(x,y);
     }
